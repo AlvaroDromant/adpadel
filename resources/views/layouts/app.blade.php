da4e0a318c82d6e2f6eb5d281c2d07984ee653d8
Alvaro Dromant <alvaro.dromant@btponetec.es>
2024-06-06 14:27:39 +0200

    cambios vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://adpadel-production.up.railway.app/build/assets/app-Cu20m_ph.css">
 
 
         <!-- Scripts -->
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].[hash].js',
+                chunkFileNames: '[name].[hash].js',
+                assetFileNames: '[name].[hash].[ext]',
+            },
+        },
+    },
+    server: {
+        https: true,
+    },
 });
